be72529edabf40e594198c0d0567a22bb208532e
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-04-11 23:07:35 +0530

    turn form into controlled component

--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,34 @@
+import React, { useState } from "react";
 import { Header } from "../components";
 
-export default function create() {
+export default function Create() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  var emailList = [];
+  var i;
+
+  const handleAddMember = () => {
+    emailList[i++] = inputs.email;
+    // setInputs({email:""});
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      inputs.meetingName +
+        "\n" +
+        inputs.startDateTime +
+        "\n" +
+        inputs.endDateTime +
+        "\n",
+    );
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
@@ -11,7 +39,7 @@ export default function create() {
           </h2>
         </div>
         <div className="flex flex-col w-full max-w-sm ">
-          <form className="">
+          <form onSubmit={handleSubmit}>
             <div className="gap-9">
               <label htmlFor="meetingName" className="mx-1">
                 Meeting name
@@ -21,41 +49,57 @@ export default function create() {
                 name="meetingName"
                 id="meetingName"
                 placeholder="Meeting Name"
+                value={inputs.meetingName}
+                onChange={handleChange}
+                required
                 className=" w-full  border outline-none focus:border-black border-gray-200 rounded p-3 mb-4"></input>
 
               <label htmlFor="startDateTime" className="mx-1">
                 Starting Time
               </label>
               <input
-                type="date"
+                type="datetime-local"
                 name="startDateTime"
                 id="startDateTime"
+                value={inputs.startDateTime}
+                onChange={handleChange}
+                required
                 className=" w-full  border outline-none focus:border-black border-gray-200 rounded p-3 mb-4"></input>
 
               <label htmlFor="endDateTime" className="mx-1">
                 End Time
               </label>
               <input
-                type="date"
+                type="datetime-local"
                 name="endDateTime"
                 id="endDateTime"
+                value={inputs.endDateTime}
+                onChange={handleChange}
+                required
                 className=" w-full  border outline-none focus:border-black border-gray-200 rounded p-3 mb-4"></input>
 
-              <label htmlFor="emailList" className="mx-1">
+              <label htmlFor="email" className="mx-1">
                 Add Patricipents{" "}
               </label>
               <input
                 type="email"
-                name="emailList"
-                id="emailList"
+                name="email"
+                id="email"
                 placeholder="Add members"
+                value={inputs.email}
+                onChange={handleChange}
                 className=" w-full  border outline-none focus:border-black border-gray-200 rounded p-3 mb-6"></input>
 
-              <button className="w-fit border text-xs bg-blue-600 hover:bg-white transition hover:text-blue-600 hover:border-blue-600 text-white font-semibold rounded p-3 mb-6 ">
+              <button
+                type="button"
+                onClick={handleAddMember}
+                className="w-fit border text-xs bg-blue-600 hover:bg-white transition hover:text-blue-600 hover:border-blue-600 text-white font-semibold rounded p-3 mb-6 ">
                 Add member
               </button>
 
-              <button className="w-full border text-lg bg-blue-600 hover:bg-white transition hover:text-blue-600 hover:border-blue-600 text-white font-semibold rounded p-3">
+              <button
+                type="submit"
+                className="w-full border text-lg bg-blue-600 hover:bg-white transition hover:text-blue-600 hover:border-blue-600 text-white font-semibold rounded p-3">
                 Submit
               </button>
             </div>
