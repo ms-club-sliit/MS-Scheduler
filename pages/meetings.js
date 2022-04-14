f0d478c6d79928f1a3315882758085a0ee90f572
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-04-14 21:30:45 +0530

    implement meeting type buttons

--- pages/meetings.js
+++ pages/meetings.js
@@ -3,11 +3,24 @@ import MeetingCard from "../components/Meeting-card";
 
 export default function Meetings() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <div className="flex jus">
-        <MeetingCard />
+    <>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <div className="flex mt-6 ml-10">
+          <div className="px-2 py-1 bg-purple-700 rounded-md mr-3 text-white">
+            COMING UP
+          </div>
+          <div className="px-2 py-1 bg-gray-200 rounded-md mr-3 text-gray-600">
+            WRAPPED
+          </div>
+          <div className="px-2 py-1 bg-gray-200 rounded-md mr-3 text-gray-600">
+            DELETED
+          </div>
+        </div>
+        <div className="flex">
+          <MeetingCard />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
